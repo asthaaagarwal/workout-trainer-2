d5504aedb6caeb27746ef3005bfea256e7864339
Karan Mhatre <me@karanmhatre.com>
2025-09-23 22:34:24 +0200

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
  
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/workout-trainer-2/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
